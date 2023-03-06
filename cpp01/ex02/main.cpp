6b6bf5570018b44f0b8c039a4d93dcc2455fd2e4
gguedes <gguedes@student.42.rio>
2023-03-05 22:26:51 -0300

    main.cpp

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -3,7 +3,7 @@
 int main( void ) {
 	std::string brain = "HI THIS IS BRAIN";
 	std::string* stringPTR = &brain;
-	std::string &stringREF = brain;
+	std::string& stringREF = brain;
 
 	std::cout << &brain << std::endl;
 	std::cout << stringPTR << std::endl;
