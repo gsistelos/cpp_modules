3bc8455d0ca18c7ccb27ce57a131567fe8185685
gguedes <gguedes@student.42.rio>
2023-03-05 22:23:53 -0300

    removed comments

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -4,12 +4,10 @@ Zombie* newZombie( std::string name );
 void randomChump( std::string name );
 
 int main( void ) {
-	//gguedes zombie is allocated at the heap, thus it must be destroyed using delete
 	Zombie* gguedes = newZombie("gguedes");
 	gguedes->announce();
 	delete gguedes;
 
-	//vsergio zombie is allocated at the stack, thus it is destroyed when its out of scope
 	randomChump("vsergio");
 	return 0;
 }
