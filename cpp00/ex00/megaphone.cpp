84770d48ba558ac8870b97f66f4e282d88b1bd34
Gabriel Sistelos <gguedes@student.42.rio>
2023-04-20 14:27:29 -0300

    refactor cpp00/ex00

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 
-static void to_upper_case( char *argv ) {
-	for (int i = 0; argv[i]; i++) {
-		argv[i] = std::toupper(argv[i]);
+static void to_upper_case(char *str)
+{
+	for (int i = 0; str[i]; i++) {
+		str[i] = std::toupper(str[i]);
 	}
 }
 
-int main( int argc, char **argv ) {
+int main(int argc, char **argv)
+{
 	if (argc == 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return 0;
 	}
+
 	for (int i = 1; i < argc; i++) {
 		to_upper_case(argv[i]);
 		std::cout << argv[i];
