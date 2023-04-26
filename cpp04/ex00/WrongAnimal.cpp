e2ad80d05449d230dfd4c899f0ffb5e0d4ddacdc
Gabriel Sistelos <gguedes@student.42.rio>
2023-04-26 17:41:02 -0300

    added wrongAnimal and tests

---
+++ cpp04/ex00/WrongAnimal.cpp
@@ -0,0 +1,38 @@
+#include "WrongAnimal.hpp"
+
+WrongAnimal::WrongAnimal(void) : type("WrongAnimal")
+{
+	std::cout << "WrongAnimal: Default constructor called." << std::endl;
+}
+
+WrongAnimal::WrongAnimal(const std::string &type) : type(type)
+{
+	std::cout << "WrongAnimal: type constructor called." << std::endl;
+}
+
+WrongAnimal::WrongAnimal(const WrongAnimal &other) : type(other.type)
+{
+	std::cout << "WrongAnimal: Copy constructor called." << std::endl;
+}
+
+WrongAnimal::~WrongAnimal()
+{
+	std::cout << "WrongAnimal: Destructor called." << std::endl;
+}
+
+WrongAnimal &WrongAnimal::operator=(const WrongAnimal &other)
+{
+	type = other.type;
+	std::cout << "WrongAnimal: Assignment operator called." << std::endl;
+	return *this;
+}
+
+std::string WrongAnimal::getType(void) const
+{
+	return type;
+}
+
+void WrongAnimal::makeSound(void) const
+{
+	std::cout << "WrongAnimal sound!" << std::endl;
+}
--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -1,20 +1,36 @@
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "WrongCat.hpp"
 
 int main(void)
 {
-	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
-	j->makeSound();
-	meta->makeSound();
-
-	delete meta;
-	delete i;
-	delete j;
+	{
+		const Animal* animal = new Animal();
+		const Animal* dog = new Dog();
+		const Animal* cat = new Cat();
+		std::cout << dog->getType() << " " << std::endl;
+		std::cout << cat->getType() << " " << std::endl;
+		cat->makeSound(); //will output the cat sound!
+		dog->makeSound();
+		animal->makeSound();
+		delete animal;
+		delete cat;
+		delete dog;
+	}
+	std::cout << std::endl;
+	{
+		const Animal* animal = new Animal();
+		const Animal* dog = new Dog();
+		const WrongAnimal* cat = new WrongCat();
+		std::cout << dog->getType() << " " << std::endl;
+		std::cout << cat->getType() << " " << std::endl;
+		cat->makeSound(); //will output the wrongAnimal sound!
+		dog->makeSound();
+		animal->makeSound();
+		delete animal;
+		delete cat;
+		delete dog;
+	}
 
 	return 0;
 }
