4ab88d88bed7d341f4bb2f881d3f53760f5e11e0
gsistelos <gabrielsistelos@gmail.com>
2023-06-21 12:38:45 -0300

    fix: convert accepting one argument

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -3,12 +3,10 @@
 
 int main( int argc, char** argv )
 {
-	if (argc < 2) {
+	if (argc != 2) {
 		std::cout << "usage: ./convert <literals>" << std::endl;
 		return 1;
 	}
-	for (int i = 1; argv[i] != NULL; i++) {
-		ScalarConverter::convert(argv[i]);
-	}
+	ScalarConverter::convert(argv[1]);
 	return 0;
 }
