9b07d36e5183e871be6cd279114820a336f06bfa
gsistelos <gabrielsistelos@gmail.com>
2024-03-14 12:06:03 -0300

    feat: cpp06/ex02

---
+++ cpp06/ex02/main.cpp
@@ -0,0 +1,45 @@
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+#include <cstdlib>
+#include <iostream>
+
+Base *generate(void) {
+  srand(time(0));
+  int i = rand() % 3;
+
+  if (i == 0) {
+    std::cout << "Created A" << std::endl;
+    return new A;
+  } else if (i == 1) {
+    std::cout << "Created B" << std::endl;
+    return new B;
+  }
+
+  std::cout << "Created C" << std::endl;
+  return new C;
+}
+
+void identify(Base *p) {
+  if (dynamic_cast<A *>(p))
+    std::cout << "A" << std::endl;
+  else if (dynamic_cast<B *>(p))
+    std::cout << "B" << std::endl;
+  else if (dynamic_cast<C *>(p))
+    std::cout << "C" << std::endl;
+}
+
+void identify(Base &p) { identify(&p); }
+
+int main(void) {
+  Base *p = generate();
+
+  identify(p);
+
+  identify(*p);
+
+  delete p;
+
+  return 0;
+}
