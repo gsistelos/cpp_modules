6472dee3142e04483d43a9881a7b9f529b5dc8c8
gsistelos <gabrielsistelos@gmail.com>
2024-03-15 19:13:27 -0300

    feat: ex02

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,26 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <deque>
+#include <stack>
+
+template <typename T> class MutantStack : public std::stack<T> {
+public:
+  MutantStack() : std::stack<T>() {}
+  MutantStack(const MutantStack &other) : std::stack<T>(other) {}
+
+  MutantStack &operator=(const MutantStack &other) {
+    if (this != &other)
+      std::stack<T>::operator=(other);
+    return *this;
+  }
+
+  ~MutantStack() {}
+
+  typedef typename std::deque<T>::iterator iterator;
+
+  iterator begin() { return std::stack<T>::c.begin(); }
+  iterator end() { return std::stack<T>::c.end(); }
+};
+
+#endif
