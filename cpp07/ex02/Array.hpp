fa6cfcf90b3799ab15b9c391a6ae885a681fb038
gsistelos <gabrielsistelos@gmail.com>
2024-04-25 15:20:41 -0300

    make file -I and ex02

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -5,63 +5,58 @@
 
 template <class T> class Array {
 public:
-  Array(void);
-  Array(unsigned int size);
-  ~Array();
+  Array(void) : _array(new T[0]), _size(0) {}
 
-  Array(const Array<T> &rhs);
-  Array<T> &operator=(const Array<T> &rhs);
+  Array(unsigned int size) : _array(new T[size]), _size(size) {
+    for (unsigned int i = 0; i < _size; i++) {
+      _array[i] = T();
+    }
+  }
 
-  T &operator[](unsigned int index);
+  ~Array() { delete[] _array; }
 
-  unsigned int size(void) const;
+  Array(const Array<T> &rhs) : _array(new T[rhs._size]), _size(rhs._size) {
+    for (unsigned int i = 0; i < _size; i++) {
+      _array[i] = rhs._array[i];
+    }
+  }
+
+  Array<T> &operator=(const Array<T> &rhs) {
+    if (this != &rhs) {
+      _size = rhs._size;
+
+      delete[] _array;
+      _array = new T[_size];
+
+      for (unsigned int i = 0; i < _size; i++) {
+        _array[i] = rhs._array[i];
+      }
+    }
+
+    return *this;
+  }
+
+  T &operator[](unsigned int index) const {
+    if (index >= _size) {
+      throw std::out_of_range("Index out of range");
+    }
+
+    return _array[index];
+  }
+
+  unsigned int size(void) const { return _size; }
 
 private:
   T *_array;
   unsigned int _size;
 };
 
-template <class T> Array<T>::Array(void) : _array(NULL), _size(0) {}
-
-template <class T>
-Array<T>::Array(unsigned int size) : _array(new T[size]), _size(size) {}
-
-template <class T> Array<T>::~Array() { delete[] _array; }
-
-template <class T>
-Array<T>::Array(const Array<T> &rhs)
-    : _array(new T[rhs._size]), _size(rhs._size) {
-  for (unsigned int i = 0; i < _size; i++) {
-    _array[i] = rhs._array[i];
-  }
-}
-
-template <class T> Array<T> &Array<T>::operator=(const Array<T> &rhs) {
-  if (this != &rhs) {
-    delete[] _array;
-    _array = new T[rhs._size];
-    _size = rhs._size;
-    for (unsigned int i = 0; i < _size; i++) {
-      _array[i] = rhs._array[i];
-    }
-  }
-  return *this;
-}
-
-template <class T> T &Array<T>::operator[](unsigned int index) {
-  if (index >= _size) {
-    throw std::out_of_range("Index out of range");
-  }
-  return _array[index];
-}
-
-template <class T> unsigned int Array<T>::size(void) const { return _size; }
-
 template <class T> std::ostream &operator<<(std::ostream &out, Array<T> &rhs) {
   for (unsigned int i = 0; i < rhs.size(); i++) {
     out << rhs[i] << " ";
   }
+
   return out;
 }
 
-#endif // ARRAY_HPP
+#endif /* ARRAY_HPP */
