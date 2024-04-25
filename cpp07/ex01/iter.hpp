359c74e1bd967917ed7325910265881ac60229e8
gsistelos <gabrielsistelos@gmail.com>
2024-04-25 14:56:17 -0300

    ex01

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -1,10 +1,18 @@
 #ifndef ITER_HPP
 #define ITER_HPP
 
-template <typename T> void iter(T *array, int length, void (*func)(T &)) {
-  for (int i = 0; i < length; i++) {
+#include <cstddef>
+
+template <typename T> void iter(T *array, size_t len, void func(T &)) {
+  for (size_t i = 0; i < len; i++) {
     func(array[i]);
   }
 }
 
-#endif // ITER_HPP
+template <typename T> void iter(T *array, size_t len, void func(const T &)) {
+  for (size_t i = 0; i < len; i++) {
+    func(array[i]);
+  }
+}
+
+#endif /* ITER_HPP */
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -1,17 +1,18 @@
 #include "iter.hpp"
-
 #include <iostream>
 
 void toLower(char &c) {
-  if (c >= 65 && c <= 90)
+  if (c >= 65 && c <= 90) {
     c += 32;
+  }
 }
 
 void toNegative(int &i) { i = -i; }
 
 template <typename T> void print_array(T *arr, int size) {
-  for (int i = 0; i < size; i++)
+  for (int i = 0; i < size; i++) {
     std::cout << arr[i] << " ";
+  }
   std::cout << std::endl;
 }
 
@@ -29,5 +30,6 @@ int main() {
   std::cout << "After: " << std::endl;
   print_array(arr1, 5);
   print_array(arr2, 5);
+
   return 0;
 }
